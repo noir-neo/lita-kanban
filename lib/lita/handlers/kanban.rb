3ffb3f794d19321212c69738bb313e299a9512ad
Shoma SATO <noir.neo.04@gmail.com>
2016-10-08 18:49:26 +0900

    :sparkles: ストーリー名リストを返すように

--- lib/lita/handlers/kanban.rb
+++ lib/lita/handlers/kanban.rb
@@ -1,7 +1,29 @@
+require 'tracker_api'
+
 module Lita
   module Handlers
     class Kanban < Handler
-      # insert handler code here
+      config :pivotal_tracker_token, required: true, type: String
+      config :pivotal_tracker_project_id, required: true
+
+      route /^kanban\s+(.+)\s+(.+)/i, :kanban, command: true, help: {
+        "kanban after_story_id before_story_id" => "Return stories names."
+      }
+
+      def kanban(response)
+        after_story_id = response.matches[0][0]
+        before_story_id = response.matches[0][1]
+        titles = stories(after_story_id, before_story_id).map do |s|
+          s.name
+        end.join("\n")
+        response.reply(titles)
+      end
+
+      def stories(after_story_id, before_story_id)
+        TrackerApi::Client.new(token: config.pivotal_tracker_token)
+          .project(config.pivotal_tracker_project_id)
+          .stories(after_story_id: after_story_id, before_story_id: before_story_id)
+      end
 
       Lita.register_handler(self)
     end
--- spec/lita/handlers/kanban_spec.rb
+++ spec/lita/handlers/kanban_spec.rb
@@ -1,4 +1,40 @@
 require "spec_helper"
 
 describe Lita::Handlers::Kanban, lita_handler: true do
+  let(:after_story_id) { 0 }
+  let(:before_story_id) { 3 }
+
+  it { is_expected.to route_command("kanban #{after_story_id} #{before_story_id}").to(:kanban) }
+
+  describe '#kanban' do
+    context 'when found stories' do
+      before do
+        allow_any_instance_of(Lita::Handlers::Kanban).to receive(:stories).and_return(
+          [
+            TrackerApi::Resources::Story.new(id: 1, name: "story1"),
+            TrackerApi::Resources::Story.new(id: 2, name: "story2")
+          ]
+        )
+      end
+
+      it 'return stories' do
+        send_command("kanban #{after_story_id} #{before_story_id}")
+        expect(replies.last).to eq "story1\nstory2"
+      end
+    end
+
+    context 'when not found stories' do
+      before do
+        allow_any_instance_of(Lita::Handlers::Kanban).to receive(:stories).and_return(
+          [
+          ]
+        )
+      end
+
+      it 'return stories' do
+        send_command("kanban #{after_story_id} #{before_story_id}")
+        expect(replies.last).to eq ""
+      end
+    end
+  end
 end
